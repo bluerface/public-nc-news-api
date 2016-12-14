73d33b87ca72499ec8cd54e926c881ee4b6d779f
bluerface <fallingsilkworm@gmail.com>
2016-12-14 15:26:09 +0000

    Add hashing & salting of password prior to db save

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new Schema({
   username: {
@@ -21,6 +22,25 @@ var UserSchema = new Schema({
     type: String,
     required: true
   }
+});
+
+UserSchema.pre('save', function (next) {
+  // generate salt
+  // hash password
+  // set the password to the hash
+  let user = this;
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      console.log(user.password);
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('users', UserSchema);
